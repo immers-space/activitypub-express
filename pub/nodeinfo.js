715b68d4fe92274d70ab3f16e3543ad0f15336af
william Murphy <william@datatitian.com>
2024-02-12 16:28:33 -0600

    cache nodeinfo user counts for 1 day to reduce DB work

--- pub/nodeinfo.js
+++ pub/nodeinfo.js
@@ -1,4 +1,18 @@
 'use strict'
+
+const USER_COUNT_FREQ = 24 * 60 * 60 * 1000
+let lastUserCountTime = 0
+let lastUserCount = Promise.resolve(0)
+
+function getUserCountWithCache (store) {
+  const now = Date.now()
+  if (lastUserCountTime + USER_COUNT_FREQ < now) {
+    lastUserCountTime = now
+    lastUserCount = store.getUserCount()
+  }
+  return lastUserCount
+}
+
 module.exports = {
   async generateNodeInfo (version) {
     return {
@@ -10,7 +24,7 @@ module.exports = {
       protocols: ['activitypub'],
       services: { inbound: [], outbound: [] },
       openRegistrations: !!this.settings.openRegistrations,
-      usage: { users: { total: await this.store.getUserCount() } },
+      usage: { users: { total: await getUserCountWithCache(this.store) } },
       metadata: this.settings.nodeInfoMetadata || {}
     }
   }
